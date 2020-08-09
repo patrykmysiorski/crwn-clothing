2d331793ab6e2562dd0ba56bd7e6d4582eefab67
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-08-09 18:23:45 +0200

    router section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
+import { Route, Switch } from "react-router-dom";
 
-const App = () => <div><Homepage/></div>;
+const HatsPage = () => (
+    <div>
+        <h1>HATS PAGE</h1>
+    </div>
+)
+
+const App = () => (
+    <div>
+        <Switch>
+            <Route exact path={'/'} component={Homepage}/>
+            <Route path={'/hats'} component={HatsPage}/>
+        </Switch>
+    </div>
+);
 
 export default App;
