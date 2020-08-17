44c373fa2429442760d1629183a0f647a0914832
patryk.mysiorski <patryk.mysiorski@gmail.com>
2020-08-17 17:22:43 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -19,10 +19,21 @@ class App extends React.Component {
     unsubscribeFromAuth = null
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-            // this.setState({currentUser: user})
-            await createUserProfileDocument(user);
-            console.log(user)
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+
+                userRef.onSnapshot(snapshot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapshot.id,
+                            ...snapshot.data()
+                        }
+                    })
+                })
+            } else {
+                this.setState({currentUser: userAuth})
+            }
         });
     }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -19,7 +19,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     const userRef = firestore.doc(`users/${userAuth.uid}`)
 
     const snapshot = await userRef.get();
-    console.log(snapshot)
+    // console.log(snapshot)
 
     if(!snapshot.exists) {
         const { displayName, email } = userAuth
