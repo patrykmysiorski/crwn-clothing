b4976c3be7241b157dcf0c0ddd8dc9997c0d6685
Patryk Mysiorski <mysior@ncdc.pl>
2020-10-13 22:02:10 +0200

    refactor function to promise

--- src/pages/homepage/shop/Shop.jsx
+++ src/pages/homepage/shop/Shop.jsx
@@ -18,15 +18,13 @@ class ShopPage extends React.Component {
 
   componentDidMount() {
     const { updateCollections } = this.props;
-    const collectionRef = firestore.collection("collections");
+    const collectionRef = firestore.collection('collections');
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
-      async (snapshot) => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        updateCollections(collectionsMap);
-        this.setState({ loading: false });
-      }
-    );
+    collectionRef.get().then((snapshot) => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
   }
 
   render() {
